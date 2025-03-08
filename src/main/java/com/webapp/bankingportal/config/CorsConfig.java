3581cb7f72d21b6e9a9230ff515280d22ab4cf5c
saravmani <saravmanicloud@gmail.com>
2025-03-08 15:43:52 +0530

    Update CorsConfig.java

--- src/main/java/com/webapp/bankingportal/config/CorsConfig.java
+++ src/main/java/com/webapp/bankingportal/config/CorsConfig.java
@@ -11,10 +11,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         
-        
     registry.addMapping("/**")
-    .allowedOrigins("https://bang-ui-prod-winnerrs-mani-dev.apps.rm1.0a51.p1.openshiftapps.com")// Should be configurable
+    .allowedOrigins("https://bang-ui-prod-winnerrs-mani-dev.apps.rm1.0a51.p1.openshiftapps.com", "https://bankapp-ui-saravanamanikandan-n-dev.apps.rm3.7wse.p1.openshiftapps.com")// Should be configurable
     .allowedMethods("GET", "POST", "PUT", "DELETE");
+
+
+        
 
         // Below should be changed for produciton
         // registry.addMapping("/**")
